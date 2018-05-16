8228e67502f7b116ab33bddea2d9ec90ff42a446
Marian Beermann <public@enkore.de>
2018-05-16 20:22:21 +0200

    link analysis

--- src/potstats2/db.py
+++ src/potstats2/db.py
@@ -1,4 +1,6 @@
 import sys
+from urllib.parse import urlparse
+from time import perf_counter
 
 from sqlalchemy import create_engine, Column, ForeignKey, Integer, Unicode, UnicodeText, Boolean, TIMESTAMP, CheckConstraint, func
 from sqlalchemy.orm import sessionmaker, relationship
@@ -43,21 +45,45 @@ def create_schema():
 def analytics():
     session = get_session()
     session.query(QuoteRelation).delete()
+    session.query(PostLinks).delete()
+    session.query(LinkRelation).delete()
 
     num_posts = session.query(Post).count()
-    with ElapsedProgressBar(length=num_posts, label='Analyzing quote relationships') as bar:
+    with ElapsedProgressBar(length=num_posts, label='Analyzing posts') as bar:
         for post in chunk_query(session.query(Post), Post.pid, chunk_size=10000):
             analyze_post(session, post)
             bar.update(1)
-    print('Analyzed {} posts in {:.1f} s ({:.0f} posts/s), discovering {} quote relationships and {} quotes.'
+    print('Analyzed {} posts in {:.1f} s ({:.0f} posts/s),\n'
+          'discovering {} quote relationships and {} quotes.'
           .format(num_posts, bar.elapsed, num_posts / bar.elapsed,
                   session.query(QuoteRelation).count(), session.query(func.sum(QuoteRelation.count)).one()))
+
+    analyze_post_links(session)
+
     session.commit()
+
+
+def analyze_post_links(session):
+    # poor man's REFRESH MATERIALIZED VIEW
+    t0 = perf_counter()
+    q = (
+        session
+        .query(PostLinks.domain, User.uid, func.sum(PostLinks.count))
+        .join('post', 'poster')
+        .group_by(PostLinks.domain, User.uid)
+    )
+    stmt = insert(LinkRelation.__table__).from_select(['domain', 'uid', 'count'], q)
+    session.execute(stmt)
+    elapsed = perf_counter() - t0
+    print('Aggregated {} links into {} link relationships in {:.1f} s.'
+          .format(session.query(PostLinks).count(), session.query(LinkRelation).count(), elapsed))
 
 
 def analyze_post(session, post):
     in_tag = False
+    capture_contents = False
     current_tag = ''
+    tag_contents = ''
     quote_level = 0
 
     def update_edge(quote_tag, poster_uid):
@@ -84,6 +110,25 @@ def analyze_post(session, post):
         )
         session.execute(stmt)
 
+    def update_url(url, post):
+        if url:
+            if url[0] == url[-1] and url[0] in ("'", '"'):
+                url = url[1:-1]
+            if '://' not in url:
+                url = 'http://' + url
+        try:
+            domain = urlparse(url).netloc
+        except ValueError:
+            print('PID %d: Could not parse URL: %r' % (post.pid, url))
+            return
+
+        stmt = insert(PostLinks.__table__).values(pid=post.pid, url=url, domain=domain, count=1)
+        stmt = stmt.on_conflict_do_update(
+            index_elements=PostLinks.__table__.primary_key.columns,
+            set_=dict(count=stmt.excluded.count + PostLinks.__table__.c.count)
+        )
+        session.execute(stmt)
+
     if not post.content:
         return
 
@@ -101,8 +146,19 @@ def analyze_post(session, post):
 
             if quote_level == 1 and current_tag.startswith('quote='):
                 update_edge(current_tag, post.poster_uid)
+
+            if quote_level == 0:
+                if current_tag.startswith('url='):
+                    update_url(current_tag[4:], post)
+                elif current_tag == 'url':
+                    capture_contents = True
+                elif current_tag == '/url' and capture_contents:
+                    update_url(tag_contents, post)
+                    capture_contents = False
         elif in_tag:
             current_tag += char
+        elif capture_contents:
+            tag_contents += char
 
 
 Base = declarative_base()
@@ -253,3 +309,25 @@ class QuoteRelation(Base):
     quotee = relationship('User', foreign_keys=quotee_uid)
 
     count = Column(Integer, default=0)
+
+
+class LinkRelation(Base):
+    __tablename__ = 'link_relation'
+
+    uid = Column(Integer, ForeignKey('users.uid'), primary_key=True)
+    domain = Column(Unicode, primary_key=True)
+
+    count = Column(Integer, default=0)
+
+    user = relationship('User')
+
+
+class PostLinks(Base):
+    __tablename__ = 'post_links'
+
+    pid = Column(Integer, ForeignKey('posts.pid'), primary_key=True)
+    url = Column(Unicode, primary_key=True)
+    domain = Column(Unicode)
+    count = Column(Integer, default=0)
+
+    post = relationship('Post')

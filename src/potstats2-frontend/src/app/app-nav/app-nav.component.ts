24bd6ddef84cfd5a6aa41a826d03b04cf773b723
audax <kadenbach@qabel.de>
2018-07-11 18:33:46 +0200

    WIP

--- src/potstats2-frontend/src/app/app-nav/app-nav.component.ts
+++ src/potstats2-frontend/src/app/app-nav/app-nav.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import {delay, map} from 'rxjs/operators';
+import {delay, filter, map, mergeMap} from 'rxjs/operators';
 import {GlobalFilterStateService} from '../global-filter-state.service';
 import {BoardsService} from '../data/boards.service';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -24,6 +25,8 @@ export class AppNavComponent implements OnInit {
   selectedBoard: number;
 
   constructor(private breakpointObserver: BreakpointObserver,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,
               private stateService: GlobalFilterStateService,
               private boardsService: BoardsService) {
     const now = (new Date()).getFullYear();
@@ -37,6 +40,18 @@ export class AppNavComponent implements OnInit {
     this.stateService.setBoard($event.value);
   }
   ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) { route = route.firstChild; }
+        return route;
+      }),
+      filter(route => route.outlet === 'primary'),
+      map(route => route.snapshot.paramMap)
+  ).subscribe((data) => {
+    console.log(data);
+    });
     this.stateService.state.pipe(
       // We delay the update to another cycle in case another components changes the year while initialising
       delay(0)

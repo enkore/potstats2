fbc167b1ed82913dd946ce6a38b9d0a425f106a7
audax <kadenbach@qabel.de>
2018-07-28 21:48:05 +0200

    Use base url instead of hardcoded url for contact info service

--- src/potstats2-frontend/src/app/data/contact-info.service.ts
+++ src/potstats2-frontend/src/app/data/contact-info.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
 import {ContactInfo} from "./types";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactInfoService {
 
-  private uri = 'http://potstats2.enkore.de/api/imprint'
+  private uri = environment.backend + '/api/imprint';
 
   constructor(private http: HttpClient) {
   }

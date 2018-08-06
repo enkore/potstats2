ae9f404af9c333aee25cbbadae3ed05306014e4e
Marian Beermann <public@enkore.de>
2018-08-06 13:50:20 +0200

    include frontend.version file into frontend dist

--- src/potstats2-frontend/pack-dist.sh
+++ src/potstats2-frontend/pack-dist.sh
@@ -3,6 +3,7 @@
 rm -rf dist/potstats2-frontend
 ng build --prod
 cd dist/potstats2-frontend
+git describe > frontend.version
 gzip -9 --keep *.js *.css
 cd ..
 tar czvf potstats2-frontend.tar.gz potstats2-frontend

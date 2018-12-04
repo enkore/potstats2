enkore/potstats2

commit 449f33d4bcb394d2ef4a9b635ef9a2f1a98f90d3

search: use _id field in elasticsearch

src/potstats2/backend/__init__.py
@@ -396,7 +396,7 @@ def search():
     if type == 'post':
         results = [dict(
             score=r['_score'],
-            pid=r['_source']['pid'],
+            pid=int(r['_id']),  # elasticsearch stores _id always as a string
             poster_uid=r['_source']['poster_uid'],
             snippet=' … '.join(r['highlight']['content']),
         ) for r in es_result['hits']['hits']]
@@ -418,7 +418,7 @@ def search():
             score=r['_score'],
             title=r['_source']['title'],
             subtitle=r['_source']['subtitle'],
-            tid=r['_source']['tid'],
+            tid=int(r['_id']),
         ) for r in es_result['hits']['hits']]
 
     td = time.perf_counter() - t0

src/potstats2/analytics.py
@@ -106,7 +106,7 @@ def elasticsearch_pusher(queue):
         bodies = queue.get()
         if bodies is ESP_POISON:
             break
-        actions = [dict(_index='post', _type='post', _source=body) for body in bodies]
+        actions = [dict(_index='post', _type='post', _id=body.pop('pid'), _source=body) for body in bodies]
         elasticsearch.helpers.bulk(es, actions, chunk_size=10000, max_chunk_bytes=100 * 1024 * 1024)
 
 
@@ -236,10 +236,6 @@ def analyze_posts(session, state_file):
                             'type': 'text',
                             'analyzer': 'german',
                         },
-                        'pid': {
-                            'type': 'integer',
-                            'index': False,
-                        },
                         'poster_uid': {
                             'type': 'integer',
                         }
@@ -300,10 +296,6 @@ def index_threads(session):
                         'type': 'text',
                         'analyzer': 'german',
                     },
-                    'tid': {
-                        'type': 'integer',
-                        'index': False,
-                    },
                 }
             }
         }
@@ -311,7 +303,7 @@ def index_threads(session):
 
     t0 = perf_counter()
     threads = session.query(Thread.tid, Thread.title, Thread.subtitle).all()
-    actions = [dict(_index='thread', _type='thread', _source=thread._asdict()) for thread in threads]
+    actions = [dict(_index='thread', _type='thread', _id=thread.tid, _source=thread._asdict()) for thread in threads]
     elasticsearch.helpers.bulk(es, actions, chunk_size=200000, max_chunk_bytes=100 * 1024 * 1024)
     es.indices.refresh('thread')
     elapsed = perf_counter() - t0

commit 62112431567e1e03af47e5f2a6248d7b75c9e209

backend.search: sorting fix
regression from 449f33d4bc

src/potstats2/backend/__init__.py
@@ -337,20 +337,18 @@ def search():
     if type not in ('post', 'thread'):
         raise APIError('Invalid value for type: %r' % type)
 
-    oid = 'pid' if type == 'post' else 'tid'
-
     # sue me, mccabe
 
     if sort == 'score':
         sorting = ['_score']
     elif sort == 'date-asc':
         sorting = [
-            {oid: {'order': 'asc'}},
+            {'_id': {'order': 'asc'}},
             '_score',
         ]
     elif sort == 'date-desc':
         sorting = [
-            {oid: {'order': 'desc'}},
+            {'_id': {'order': 'desc'}},
             '_score',
         ]
     else:
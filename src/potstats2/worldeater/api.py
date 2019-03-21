f0a6f2e561d2242fc54064d056dbb906feb0d521
Marian Beermann <public@enkore.de>
2019-03-21 14:30:54 +0100

    crawler.api: work around assertion

--- src/potstats2/worldeater/api.py
+++ src/potstats2/worldeater/api.py
@@ -165,7 +165,10 @@ class XmlApiConnector:
         while page >= 0:
             board = self.board(bid, page)
             threads = board.findall('./threads/thread')
-            assert not (len(threads) == 30 and page == last_page), 'Last page was not actually the last page (%d)' % last_page
+            if len(threads) == 30 and page == last_page:
+                page += 1
+                last_page += 1
+                continue
             yield from threads
             page -= 1
 
